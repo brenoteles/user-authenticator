1628d931b8acae81f60771b952f1290dbb0525f5
brenoteles <49119963+brenoteles@users.noreply.github.com>
2021-03-14 11:35:37 +0000

    New comments

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ const cors = require('cors');
 const express = require('express');
 const routes = require('./routes');
 
-require('./database'); //Importando a base de dados
+require('./database'); //Importing DB
 
 const app = express();
 
 app.use(express.json());
-app.use(cors());
-app.use(routes);
+app.use(cors()); //Cross-origin resource sharing
+app.use(routes); //Calling routes
 app.listen(3333);
